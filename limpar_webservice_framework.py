38650ed166147b5819fd67f58d8bc96fe874ad60
felipeffm <felipefm@id.uff.br>
2020-03-21 19:14:25 -0300

    todos

--- limpar_webservice_framework.py
+++ limpar_webservice_framework.py
@@ -1,3 +1,29 @@
 import selenium
 import bs4
 #atualizado!!!!
+
+
+//TODO aquisição de dados historicos
+//TODO analise de dados historicos
+//TODO aquisição de dados em tempo real
+//TODO comparação de dados em tempo real com resultados de analise historica
+//TODO decisao de compra ou venda
+//TODO acesso site e login de acesso
+
+
+
+//TODO integracao compra ou venda webservices
+
+#clicar no botao de compra ou de venda
+
+#encontrar caixas de texto para 
+##quantidade
+##preco limite
+##assinatura eletrônica caso não esteja clicado "salvar"
+##botão de concluir acao
+
+#checar se elementos estão ok
+
+#estando ok, interagir com elementos
+
+
