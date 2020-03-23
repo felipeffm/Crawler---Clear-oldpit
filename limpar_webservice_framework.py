003f6c4059e46ad0a4efd05092d46130c226e2fb
felipeffm <felipefm@id.uff.br>
2020-03-22 21:37:10 -0300

    iframe_vencido

--- limpar_webservice_framework.py
+++ limpar_webservice_framework.py
@@ -2,7 +2,7 @@
 
 # //TODO aquisição de dados historicos
 # //TODO analise de dados historicos
-# //TODO aquisição de dados em tempo real
+        # //TODO aquisição de dados em tempo real
 # //TODO comparação de dados em tempo real com resultados de analise historica
 # //TODO decisao de compra ou venda
 # //TODO acesso site e login de acesso
@@ -10,46 +10,35 @@
 
 
 # //TODO integracao compra ou venda webservices
-"""Script para instalação geckodriver no ubuntu--> install_gecko.sh"""
-#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
-#from selenium.webdriver.firefox.options import Options
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-#binary = FirefoxBinary('/snap/bin/firefox')
-from selenium.webdriver.firefox.options import Options
-opts = Options()
-opts.log.level = "trace"
-#options = Options()
-#options.add_argument("-profile")
-# put the root directory your default profile path here, you can check it by opening Firefox and then pasting 'about:profiles' into the url field 
-#options.add_argument("/firefox_profile")
-browser=webdriver.Firefox(executable_path = 'geckodriver',options=opts)
-#from selenium.webdriver.chrome.options import Options
-#options = Options()
-#options.binary_location = r'/bin/'
-#driver = webdriver.Chrome('chromedriver', options = options)
-#entrar no site
+
+
 def abrir_navegador():
     global driver
-    
-    #web e é argumento do objeto que é o próprio navegador.
-    #ptions = Options()
-    #ptions.add_argument("--user-data-dir=/usr/lib/chromium-browser")
-    #options.add_argument("--headless") 
 
-#    driver = webdriver.Firefox()
     driver = webdriver.Firefox()
-    time.sleep(3)
-    #Acesso do site wpp web pelo navegador
-    driver.get('https://web.whatsapp.com/')
-    #tempo de espera para carregar a página do wpp web.
-    time.sleep(10)
-#clicar no botao de compra ou de venda
+    driver.get(r'https://novopit.clear.com.br/Operacoes/RendaVariavel/Basico')
+    #usuario loga manualmente na clear
+abrir_navegador()
+    
+""" esse html não ta com o xpath funcionando pq tem muitos eventos e o css ta 
+carregado demais. Então a seleção vai ser por partes, primeiro as hierarquias maiores"""
+
+lista_iframes = driver.find_elements_by_tag_name('iframe')
+iframe = lista_iframes[2]
+driver.switch_to.frame(iframe)
+body = driver.find_element_by_tag_name('body')
+lista_acoes = body.find_elements_by_class_name('asset-name')
 
 #encontrar caixas de texto para 
+import bs4
 ##quantidade
+//*[@id="input-quantity"]
 ##preco limite
+//*[@id="input-price"]
 ##assinatura eletrônica caso não esteja clicado "salvar"
+<input type="password" placeholder="Assinatura Eletr." class="input-digital-sign-field" maxlength="7">
 ##botão de concluir acao
 
 #checar se elementos estão ok
